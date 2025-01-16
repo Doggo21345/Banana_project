47094a118b1be5aa241422015ea2df999ed803be
Arya Panda <a12pandaa@gmail.com>
2025-01-16 10:21:16 -0600

    fixing the ML part

--- ML part.R
+++ ML part.R
@@ -11,7 +11,6 @@ pacman::p_load(tidyverse, ggplot2, gganimate, flexdashboard, mosaic, moderndive,
 install.packages("xgboost")
 library(xg)
 
-# Prepare data for modeling
 consumption_date <- consumption_date %>%
   mutate(date_id = as.integer(format(as.Date(date, "%Y-%m-%d"), "%Y%m%d")))
 
@@ -39,9 +38,7 @@ mse <- test_data %>%
   pull(MSE)
 
 
-mse
-mae
-# Visualize predictions
+
 test_data <- test_data %>%
   mutate(index = row_number())
 
@@ -61,39 +58,27 @@ ggplot(test_data, aes(x = index)) +
 
 
 
-
-
-# Assume `Consumption_Log` is already loaded into the environment as a data frame
 data <- Consumption_Log_original
 view(data)
 view(Consumption_Log_original)
 
-# Ensure date_id is in Date format if needed
-data$date_id <- as.Date(as.character(data$date_id), format = "%Y%m%d")
-
-# Add is_weekend column based on date_id
-data$is_weekend <- as.integer(weekdays(data$date_id) %in% c("Saturday", "Sunday"))
-
-# Ensure time_period is a factor with predefined levels
-
-# One-hot encode time_period
 time_dummies <- model.matrix(~ time_period - 1, data)
 data <- cbind(data, time_dummies)
 
-set.seed(123) # Ensure reproducibility
+set.seed(123) 
 train_index <- createDataPartition(data$bananas_consumed, p = 0.8, list = FALSE)
 train_data <- data[train_index, ]
 test_data <- data[-train_index, ]
 
 
 
-# Define a formula dynamically for simplicity
+
 predictors <- colnames(time_dummies)
 formula <- as.formula(paste("bananas_consumed ~ is_weekend +", paste(predictors, collapse = " + ")))
 
 
 
-write.csv("")
+
 
 # Linear Regression
 lm_model <- train(
@@ -109,7 +94,7 @@ lm_model <-  train(
 )
 
 
-# Support Vector Machine
+
 svm_model <- train(
   formula, data = train_data, method = "svmRadial", 
   trControl = trainControl(method = "cv", number = 10)
@@ -117,7 +102,6 @@ svm_model <- train(
 svm_preds <- predict(svm_model, test_data)
 svm_rmse <- RMSE(svm_preds, test_data$bananas_consumed)
 
-# Random Forest
 rf_model <- train(
   formula, data = train_data, method = "rf", 
   trControl = trainControl(method = "cv", number = 10)
@@ -125,7 +109,7 @@ rf_model <- train(
 rf_preds <- predict(rf_model, test_data)
 rf_rmse <- RMSE(rf_preds, test_data$bananas_consumed)
 
-# Gradient Boosting
+
 xgb_model <- train(
   formula, data = train_data, method = "xgbTree", 
   trControl = trainControl(method = "cv", number = 10)
@@ -133,7 +117,7 @@ xgb_model <- train(
 xgb_preds <- predict(xgb_model, test_data)
 xgb_rmse <- RMSE(xgb_preds, test_data$bananas_consumed)
 
-# Neural Network
+
 nn_model <- train(
   formula, data = train_data, method = "nnet", 
   trControl = trainControl(method = "cv", number = 10), 
@@ -142,7 +126,7 @@ nn_model <- train(
 nn_preds <- predict(nn_model, test_data)
 nn_rmse <- RMSE(nn_preds, test_data$bananas_consumed)
 
-# Model Comparison
+
 results <- data.frame(
   Model = c("Linear Regression", "SVM", "Random Forest", "Gradient Boosting", "Neural Network"),
   RMSE = c(lm_rmse, svm_rmse, rf_rmse, xgb_rmse, nn_rmse)
